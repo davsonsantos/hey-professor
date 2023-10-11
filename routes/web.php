2211ea4914016a95b0a24503a5b0f54eebd36aea
Davson Santos <davsonsantos@gmail.com>
2023-10-11 15:55:05 -0300

    HEY-10: table like request

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,6 @@
 <?php
 
-use App\Http\Controllers\{DashboardController, ProfileController, QuestionController};
+use App\Http\Controllers\{DashboardController, ProfileController, Question, QuestionController};
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
@@ -17,6 +17,7 @@ Route::get('/', function () {
 Route::get('/dashboard', DashboardController::class)->middleware(['auth', 'verified'])->name('dashboard');
 
 Route::post('/questions/create', [QuestionController::class, 'store'])->name('questions.store');
+Route::post('/questions/like/{question}', [Question\LikeController::class])->name('questions.store');
 
 Route::middleware('auth')->group(function () {
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
